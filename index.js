82c7831b5cee13a238ff9de851e4d9acb342a23f
Alan <a386226@gmail.com>
2018-10-03 14:30:04 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const passport = require('./config/passport')()
+const express = require('express')
+const parser = require('body-parser')
+const cors = require('cors')
+
+
+const userController = require('./controllers/users.js')
+const postController = require('./controllers/posts.js')
+
+const app = express()
+
+app.use(passport.initialize())
+app.use(cors())
+app.use(parser.json())
+
+app.use('/users', userController)
+app.use('/api/posts', postController)
+
+
+app.listen(3001, () => console.log('Listening on port 3001 :)'))
