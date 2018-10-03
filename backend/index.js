69483be47b3ef0fdaf2b2b805f094b6576c52689
okikokim808 <kikomanistheman15@gmail.com>
2018-10-03 15:42:16 -0700

    fixed auth

--- backend/index.js
+++ backend/index.js
@@ -16,5 +16,5 @@ app.use(parser.json())
 app.use('/users', userController)
 app.use('/api/posts', postController)
 
-module.exports.UserPost = require('./UserPost')
+module.exports.UserPost = require('./models/UserPost')
 app.listen(3001, () => console.log('Listening on port 3001 :)'))
