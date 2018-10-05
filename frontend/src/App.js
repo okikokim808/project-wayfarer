ff1263c1cbc102b3f40397d83bad446b8eb4c7c4
Luke <luke@Lukes-MacBook-Pro.local>
2018-10-05 09:42:18 -0700

    added seed data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-=======
 import About from './About';
 import Carousel from './Carousel';
-import Splashnav from './Splashnav'
->>>>>>> 435f369da1f11d06052f7b1d744a9084c7cfcee9
+// import Splashnav from './Splashnav';
+import Loggedinnav from './Loggedinnav'
+import Citylistcontainer from './containers/Citylistcontainer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Splashnav />
-        <Carousel />
-      
-        <About />
+        <Citylistcontainer />
         
           
        
